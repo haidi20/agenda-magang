923f674baebc0e3d3a62086f53a8ccdbe286123d
haidi20 <nhaidii@yahoo.com>
2017-07-17 07:46:14 +0800

    lagi

--- routes/web.php
+++ routes/web.php
@@ -11,35 +11,33 @@
 |
 */
 
-Route::group(['middleware' => 'Validproxies'],function(){
-  // Route::group(['middleware' => 'login'],function(){
-    Route::get('/', function () {
-      if(Auth::check()){
-        return redirect('home/' . Auth::id());
-      }
-        return view('index.login');
-    });
-    Route::post('/login' , 'LoginController@login');
-    Route::get('/logout', 'LoginController@logout');
-  // });
 
-  Route::get('/excel/{id}','ExcelController@store');
-
-  Route::group(['middleware' => 'user'],function(){
-    Route::get('/home/{id}', 'AgendaController@index');
-    Route::get('/agenda', function () {
-        return view('index.tambah_agenda');
-    });
-    Route::post('/tambah/agenda','AgendaController@store');
+  Route::get('/', function () {
+    if(Auth::check()){
+      return redirect('home/' . Auth::id());
+    }
+      return view('index.login');
   });
+  Route::post('/login' , 'LoginController@login');
+  Route::get('/logout', 'LoginController@logout');
+// });
 
-  Route::group(['middleware' => 'admin'],function(){
-    Route::post('/tambah/user', 'UserController@store');
-    Route::post('/edit/user' , 'UserController@edit');
-    Route::post('/delete/user', 'UserController@destroy');
-    Route::post('/update/user' ,'UserController@update');
-    Route::get('/lihat/user', function () {
-        return view('index.table_user');
-    });
+Route::get('/excel/{id}','ExcelController@store');
+
+Route::group(['middleware' => 'user'],function(){
+  Route::get('/home/{id}', 'AgendaController@index');
+  Route::get('/agenda', function () {
+      return view('index.tambah_agenda');
+  });
+  Route::post('/tambah/agenda','AgendaController@store');
+});
+
+Route::group(['middleware' => 'admin'],function(){
+  Route::post('/tambah/user', 'UserController@store');
+  Route::post('/edit/user' , 'UserController@edit');
+  Route::post('/delete/user', 'UserController@destroy');
+  Route::post('/update/user' ,'UserController@update');
+  Route::get('/lihat/user', function () {
+      return view('index.table_user');
   });
 });
