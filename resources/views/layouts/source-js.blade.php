4387d2825cdb70640f3add322ada1283d98e197b
haidi20 <nhaidii@yahoo.com>
2017-07-17 00:41:38 +0800

    secureee

--- resources/views/layouts/source-js.blade.php
+++ resources/views/layouts/source-js.blade.php
@@ -1,15 +1,15 @@
 <!-- Jquery Core Js -->
-<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{secure_asset('plugins/jquery/jquery.min.js')}}"></script>
 
 <!-- Bootstrap Core Js -->
-<script src="{{asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
+<script src="{{secure_asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
 
 <!-- Waves Effect Plugin Js -->
-<script src="{{asset('plugins/node-waves/waves.js')}}"></script>
+<script src="{{secure_asset('plugins/node-waves/waves.js')}}"></script>
 
 <!-- Validation Plugin Js -->
-<script src="{{asset('plugins/jquery-validation/jquery.validate.js')}}"></script>
+<script src="{{secure_asset('plugins/jquery-validation/jquery.validate.js')}}"></script>
 
 <!-- Custom Js -->
-<script src="{{asset('js/admin.js')}}"></script>
-<script src="{{asset('js/pages/examples/sign-in.js')}}"></script>
+<script src="{{secure_asset('js/admin.js')}}"></script>
+<script src="{{secure_asset('js/pages/examples/sign-in.js')}}"></script>
