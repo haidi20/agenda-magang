d8ecdd628e6b249c017daf007878a626a20113c1
haidi20 <nhaidii@yahoo.com>
2017-07-17 08:41:12 +0800

    menyesuaikan postgreSQL

--- database/seeds/UsersSeedTable.php
+++ database/seeds/UsersSeedTable.php
@@ -13,8 +13,6 @@ class UsersSeedTable extends Seeder
     public function run()
     {
 
-      DB::statement("TRUNCATE TABLE agenda");
-      DB::statement("TRUNCATE TABLE users CASCADE");
       DB::statement('SET FOREIGN_KEY_CHECKS=0');
       DB::table('users')->truncate();
       DB::table('users')->insert([
@@ -40,5 +38,7 @@ class UsersSeedTable extends Seeder
           'jabatan'   => 'anggota',
         ]
       ]);
+      DB::statement("TRUNCATE TABLE agenda");
+      DB::statement("TRUNCATE TABLE users CASCADE");
     }
 }
