401d3b8445c3ec74f79781973cdf4c11d9cb699c
haidi20 <nhaidii@yahoo.com>
2017-07-23 20:25:59 +0800

    tanpa .env dan merapikan route

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,10 @@
 |
 */
 
-Route::group(['middleware' => 'login'], function(){
-  Route::get('/',function(){ return view('index.login'); });
-});
+Route::get('/',['middleware' => 'login' , function(){ return view('index.login'); }]);
 Route::post('/login' , 'LoginController@login');
 Route::get('/logout', 'LoginController@logout');
 Route::group(['middleware' => 'user'],function(){
   Route::resource('/agenda','AgendaController');
-  Route::group(['middleware' => 'admin'],function(){
-    Route::resource('/user', 'UserController');
-  });
+  Route::group(['middleware' => 'admin'],function(){ Route::resource('/user', 'UserController'); });
 });
