71f2b6c33d60007273b66071beefe2c55bd924a9
faizaln <faizalnabil09@gmail.com>
2017-08-08 14:45:21 +0800

    catatan baru

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-// catatan dari haidi di master 
+// catatan dari haidi di master
+// catatan faizal
 Route::get('/',['middleware' => 'login' , function(){ return view('index.login'); }]);
 Route::post('login' , 'LoginController@login');
 Route::get('logout', 'LoginController@logout');
