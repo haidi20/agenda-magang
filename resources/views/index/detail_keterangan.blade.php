30c0299f82bec7d0c63f3abc39b2bfea82134d84
haidi20 <nhaidii@yahoo.com>
2017-08-09 13:36:51 +0800

    update dari haid

---
+++ resources/views/index/detail_keterangan.blade.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="myModalket" role="dialog">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Detail keterangan</h4>
+      </div>
+	  <div class="modal-body">
+		<p id="modal_detail_keterangan">
+
+		</p>
+	  </div>
+      <div class="modal-footer">
+        <input name="id" type="hidden" class="form-control" id="id_delete_modal" >
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+	</div>
+  </div>
+</div>
